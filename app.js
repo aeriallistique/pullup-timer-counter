be8a57012628913d30442881b248f5cea08f3ff9
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2025-03-20 11:32:34 +0000

    refactored code, fixed styling

--- app.js
+++ app.js
@@ -1,73 +1,70 @@
-function initialize(){
+function initialize() {
   //  helper funtions to grab DOM elements
-  const $ = (item)=>{return document.getElementById(item)};
-  const $$ = (item)=>{return document.querySelector(item)}
+  const $ = (item) => { return document.getElementById(item); };
+  const $$ = (item) => { return document.querySelector(item); };
 
   // grab DOM elements
-  const timerBox = $('timerBox')
-  const secondsBox = $('secondsBox')
-  const minutesBox = $('minutesBox')
-  const hoursBox = $('hoursBox')
-  const startBtn = $('start')
-  const stopBtn = $('stop')
-  const pauseBtn = $('pause')
+  const timerBox = $('timerBox');
+  const secondsBox = $('secondsBox');
+  const minutesBox = $('minutesBox');
+  const hoursBox = $('hoursBox');
+  const startBtn = $('start');
+  const stopBtn = $('stop');
+  const pauseBtn = $('pause');
   const body = document.body;
   const ding = $$('.ding');
-  const repsNumberSpan = $$('.repsNumberSpan')
+  const repsNumberSpan = $$('.repsNumberSpan');
   const howManyLeftButton = $('howManyLeft');
   const timeLeftDisplay = $('timeLeftDisplay');
   const howManyPullupsLeftDisplay = $('howManyPullupsLeft');
   const paceNeededDisplay = $('paceNeeded');
-  
+
   const pullups = +10000;
   const keyCodesAndNumbers = [
-    {code:49, num:1},
-    {code:50, num:2},
-    {code:51, num:3},
-    {code:52, num:4},
-    {code:53, num:5},
-    {code:54, num:6},
-    {code:55, num:7},
-    {code:56, num:8},
-    {code:57, num:9},
-    {code:48, num:10},
-    {code:77, num: 0}
-  ]
-  
+    { code: 49, num: 1 },
+    { code: 50, num: 2 },
+    { code: 51, num: 3 },
+    { code: 52, num: 4 },
+    { code: 53, num: 5 },
+    { code: 54, num: 6 },
+    { code: 55, num: 7 },
+    { code: 56, num: 8 },
+    { code: 57, num: 9 },
+    { code: 48, num: 10 },
+    { code: 77, num: 0 }
+  ];
+
   let intervalID;
   let seconds = +00;
   let minutes = +00;
   let hours = +00;
-  let numberOfReps=+0;
+  let numberOfReps = +0;
   let secondsIn24Hours = +86400;
   let pullupsRemaining;
-  let bgColor = ['white', '#92C7CF','#AAD7D9', '#FBF9F1', 
-    '#E5E1DA', '#7BD3EA', '#A1EEBD', '#F6F7C4', '#F2AFEF', 
-    '#C499F3', '#756AB6', '#AC87C5','#E0AED0','#FFE5E5',
-    '#7BD3EA','#A1EEBD','#F6F7C4', '#F6D6D6' ];
+  let bgColor = ['white', '#92C7CF', '#AAD7D9', '#FBF9F1',
+    '#E5E1DA', '#7BD3EA', '#A1EEBD', '#F6F7C4', '#F2AFEF',
+    '#C499F3', '#756AB6', '#AC87C5', '#E0AED0', '#FFE5E5',
+    '#7BD3EA', '#A1EEBD', '#F6F7C4', '#F6D6D6'];
 
-    minutesBox.innerText=`00`
-    secondsBox.innerText=`00`
-    hoursBox.innerText=`00`;
-    repsNumberSpan.innerText='0'
+  minutesBox.innerText = `00`;
+  secondsBox.innerText = `00`;
+  hoursBox.innerText = `00`;
+  repsNumberSpan.innerText = '0';
 
-    timerBox.innerText= `${hoursBox.innerText}:${minutesBox.innerText}:${secondsBox.innerText}`;
+  timerBox.innerText = `${hoursBox.innerText}:${minutesBox.innerText}:${secondsBox.innerText}`;
 
-   
+  document.addEventListener('keyup', countPullups);
+  startBtn.addEventListener('click', start);
+  stopBtn.addEventListener('click', stop);
+  pauseBtn.addEventListener('click', pause);
+  howManyLeftButton.addEventListener('click', howMuchTimeLeft);
 
-
-    document.addEventListener('keyup', countPullups)
-    startBtn.addEventListener('click', start)
-    stopBtn.addEventListener('click', stop)
-    pauseBtn.addEventListener('click', pause)
-    howManyLeftButton.addEventListener('click', howMuchTimeLeft)
- 
-  function dingNoise(){
+  function dingNoise() {
     ding.play();
   }
 
-  function changeBgEveryHalfMinute(){
-    if(seconds === 30 || seconds === 0){
+  function changeBgEveryHalfMinute() {
+    if (seconds === 30 || seconds === 0) {
       dingNoise();
 
       // background change did not look good for the camera so taken it out.
@@ -76,92 +73,92 @@ function initialize(){
     }
   }
 
-  function start(e){
+  function start(e) {
     e.preventDefault();
-    dingNoise()
-    intervalID = setInterval(()=>{
+    dingNoise();
+    intervalID = setInterval(() => {
       secondsIn24Hours--;
       seconds++;
-      changeBgEveryHalfMinute()
-      minutes <10 ? minutesBox.innerText=`0${minutes}` : minutesBox.innerText=`${minutes}`
-      seconds < 10 ? secondsBox.innerText=`0${seconds}`: secondsBox.innerText=`${seconds}`
+      changeBgEveryHalfMinute();
+      minutes < 10 ? minutesBox.innerText = `0${minutes}` : minutesBox.innerText = `${minutes}`;
+      seconds < 10 ? secondsBox.innerText = `0${seconds}` : secondsBox.innerText = `${seconds}`;
 
-      if(seconds > 59){ 
+      if (seconds > 59) {
         minutes++;
-        seconds=0; 
-        changeBgEveryHalfMinute()
-        secondsBox.innerText = `0${seconds}`
+        seconds = 0;
+        changeBgEveryHalfMinute();
+        secondsBox.innerText = `0${seconds}`;
       }
-      if(minutes > 59){
-        minutes=0;
-        minutesBox.innerText=`0${minutes}`;
+      if (minutes > 59) {
+        minutes = 0;
+        minutesBox.innerText = `0${minutes}`;
         hours++;
-        hoursBox.innerText = `0${hours}`
+        hoursBox.innerText = `0${hours}`;
       }
 
-      timerBox.innerText= `${hoursBox.innerText}:${minutesBox.innerText}:${secondsBox.innerText}`;
+      timerBox.innerText = `${hoursBox.innerText}:${minutesBox.innerText}:${secondsBox.innerText}`;
 
 
-    },1000)
-    startBtn.disabled=true;
-    pauseBtn.disabled=false;
+    }, 1000);
+    startBtn.disabled = true;
+    pauseBtn.disabled = false;
 
   }
 
-  function stop(){
-    startBtn.disabled=false;
+  function stop() {
+    startBtn.disabled = false;
     clearInterval(intervalID);
     seconds = +0;
     minutes = +0;
-    minutesBox.innerText=`00`
-    secondsBox.innerText=`00`
-    hoursBox.innerText=`00`;
-    timerBox.innerText= `${hoursBox.innerText}:${minutesBox.innerText}:${secondsBox.innerText}`;
+    minutesBox.innerText = `00`;
+    secondsBox.innerText = `00`;
+    hoursBox.innerText = `00`;
+    timerBox.innerText = `${hoursBox.innerText}:${minutesBox.innerText}:${secondsBox.innerText}`;
 
   }
 
-  function pause(){
-    clearInterval(intervalID)
-    startBtn.disabled=false;
-    pauseBtn.disabled=true;
+  function pause() {
+    clearInterval(intervalID);
+    startBtn.disabled = false;
+    pauseBtn.disabled = true;
   }
 
-  function countPullups(e){
+  function countPullups(e) {
     //press M to subtract 1 invalid pullup rep
-   if(e.keyCode === 77){
-     numberOfReps--;
-   }
+    if (e.keyCode === 77) {
+      numberOfReps--;
+    }
 
-   const found = keyCodesAndNumbers.filter((item,index)=>{
-      if(e.keyCode===item.code ){return item}
-    })
-    numberOfReps = numberOfReps + Number(found[0].num)
+    const found = keyCodesAndNumbers.filter((item, index) => {
+      if (e.keyCode === item.code) { return item; }
+    });
+    numberOfReps = numberOfReps + Number(found[0].num);
     repsNumberSpan.innerText = numberOfReps;
-    
+
     return numberOfReps;
   }
 
 
-  function howManyPullupsLeft(){
+  function howManyPullupsLeft() {
     pullupsRemaining = pullups - numberOfReps;
     howManyPullupsLeftDisplay.innerText = `Pu left: ${pullupsRemaining}`;
     return pullupsRemaining;
   }
-  
 
-  function howMuchTimeLeft(e){
+
+  function howMuchTimeLeft(e) {
     e.preventDefault();
-     const minutesRemaining = Math.floor(secondsIn24Hours / 60);
-     const secondsRemainder = secondsIn24Hours % 60;
-     const hoursRemaining = Math.floor(minutesRemaining / 60);
-     const minutesRemainder = minutesRemaining % 60;
-     const timeLeft = `Time left: ${hoursRemaining} : ${minutesRemainder}: ${secondsRemainder}`
-     timeLeftDisplay.innerHTML = timeLeft;
-     howManyPullupsLeft();
-     const pace = Math.ceil(pullupsRemaining / minutesRemaining)
-     paceNeededDisplay.innerText = `Pace needed: ${pace} / minute`;
+    const minutesRemaining = Math.floor(secondsIn24Hours / 60);
+    const secondsRemainder = secondsIn24Hours % 60;
+    const hoursRemaining = Math.floor(minutesRemaining / 60);
+    const minutesRemainder = minutesRemaining % 60;
+    const timeLeft = `Time left: ${hoursRemaining} : ${minutesRemainder}: ${secondsRemainder}`;
+    timeLeftDisplay.innerHTML = timeLeft;
+    howManyPullupsLeft();
+    const pace = Math.ceil(pullupsRemaining / minutesRemaining);
+    paceNeededDisplay.innerText = `Pace needed: ${pace} / minute`;
   }
-  
+
 };
 
 initialize();
